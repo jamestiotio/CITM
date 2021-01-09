85db0ea9c288b848a999e1e3e32d4b1d9f428946
James R T <jamestiotio@gmail.com>
2021-01-10 03:24:28 +0800

    Update cat scraper & cat list
    - Use enumerate method instead for the cat scraper since the table's source on the Fandom website might have some (typo-induced?) errors in terms of non-integers being allocated as the cat IDs
    
    - Some cats in the cat list have their names updated, on top of some additional new cats

--- tools/scraper/cat_scraper.py
+++ tools/scraper/cat_scraper.py
@@ -9,19 +9,30 @@ from bs4 import BeautifulSoup
 def main():
     r = requests.get("https://battle-cats.fandom.com/wiki/Cat_Release_Order")
     soup = BeautifulSoup(r.text, "html.parser")
-    cats = soup.find("div", id="mw-content-text").find("table", {"class":"article-table"})("tr")
-    
+    cats = soup.find("div", id="mw-content-text").find(
+        "table", {"class": "article-table"}
+    )("tr")
+
     cat_list = []
-    
-    for i in cats[2:]:
-        id = i.find_all("td")[0].text.strip().encode("ascii", "ignore").decode("ascii")
-        name = i.find_all("td")[2].text.strip().encode("ascii", "ignore").decode("ascii")
-        
-        cat_list.append({"itemId":int(id),"itemCategory":1,"amount":100,"title":str(name)})
-    
+
+    for cat_id, val in enumerate(cats[2:]):
+        # Ignore, delete/remove and omit non-ASCII characters since they could/might potentially cause some trouble (kind of)
+        name = (
+            val.find_all("td")[2].text.strip().encode("ascii", "ignore").decode("ascii")
+        )
+
+        cat_list.append(
+            {
+                "itemId": int(cat_id),
+                "itemCategory": 1,
+                "amount": 100,
+                "title": str(name),
+            }
+        )
+
     with open("cat_list.json", "w+") as f:
-        f.write(json.dumps(cat_list, sort_keys=False, indent=2))      
+        f.write(json.dumps(cat_list, sort_keys=False, indent=2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
